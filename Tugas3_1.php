23d2fb7567bad4a3be0899d1204257b1310b3b19
Albert <albert.bagil@gmail.com>
2021-11-10 18:19:49 +0700

    Tugas 3 PHP
    Tugas 3 PHP untuk dikumpulkan ke Niomic

---
+++ Tugas3_1.php
@@ -0,0 +1,10 @@
+<?php
+$jarak = 360;
+$kecepatan = 90;
+$waktu = $jarak / $kecepatan;
+
+echo "Jarak kota A - Z = $jarak Km <br>";
+echo "Kecepatan motor = $kecepatan <br>";
+echo "Waktu Tempuh = ? <br>";
+echo "Jawaban : $waktu Menit";
+ ?>
